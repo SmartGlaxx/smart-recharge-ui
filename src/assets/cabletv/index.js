9017aa1aab3844ff9f670850949f29334daeee98
SmartGlaxx <52786384+SmartGlaxx@users.noreply.github.com>
2021-08-08 14:37:18 +0100

    Delete src directory

--- src/assets/cabletv/index.js
+++
@@ -1,6 +0,0 @@
-import Dstv from './dstv.png'
-import Gotv from './gotv.jpg'
-import Startimes from './startimes.png'
-
-
-export {Dstv, Gotv, Startimes}
--- src/assets/hero/index.js
+++
@@ -1,4 +0,0 @@
-// import Image1 from './image1.png'
-import Image from './image.png'
-
-export {Image}
